73c7da9da39eadc459be4db902b4d00031664c5e
Elisa Shapiro <elisashapiro@gmail.com>
2021-06-16 20:55:17 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.querySelector("main").remove();
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+newHeader.innerHTML = "<h1#victory>YOUR-NAME is the champion</h1>";
